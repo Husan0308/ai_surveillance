d58e7dd0edff336d7a75b5a24b90634b0493dc68
Xusan <128838462+Husan0308@users.noreply.github.com>
2026-08-19 15:45:37 +0500

    Bump Sentinel UI build to r10

--- services/camera_v2/sentinel_ui.py
+++ services/camera_v2/sentinel_ui.py
@@ -23,7 +23,7 @@ from .sentinel_ui_pages import EventsPage, PeoplePage, RoomsPage
 from .sentinel_ui_settings import SettingsPage
 
 
-BUILD_TAG = "2026.08.19-r9"
+BUILD_TAG = "2026.08.19-r10"
 
 
 class MainWindow(QMainWindow):
