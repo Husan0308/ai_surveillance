8468f3d7c90bca11f511437db3f912601f195d94
Xusan <128838462+Husan0308@users.noreply.github.com>
2026-09-19 17:02:01 +0500

    insert NvDCF after YOLO26m inference

--- scripts/yolo26m_person/detection.hpp
+++ scripts/yolo26m_person/detection.hpp
@@ -3,6 +3,9 @@
 #include <nvdsmeta.h>
 #include <cmath>
 #include <dlfcn.h>
+#ifdef YOLO26_TRACKER
+#include "tracker.hpp"
+#endif
 
 struct DetectionCounter {
   std::atomic<guint64> frames{0}, persons{0}, batches{0}, errors{0}, last_unix_ms{0};
@@ -167,6 +170,17 @@ static void detection_link(GstElement *from, GstElement *tiler, GstElement *to) 
   g_object_set(filter,"caps",rgba,nullptr);gst_caps_unref(rgba);
   auto *osd=make("nvdsosd","person-overlay");
   g_object_set(osd,"process-mode",1,"display-text",TRUE,"display-bbox",TRUE,"display-mask",FALSE,"display-clock",FALSE,nullptr);
-  link(from,gie);link(gie,tiler);link(tiler,convert);link(convert,filter);link(filter,osd);link(osd,to);
+  link(from,gie);
+#ifdef YOLO26_TRACKER
+  auto *tracker=create_nvdcf_tracker();
+  link(gie,tracker);link(tracker,tiler);
+#else
+  link(gie,tiler);
+#endif
+  link(tiler,convert);link(convert,filter);link(filter,osd);link(osd,to);
+#ifdef YOLO26_TRACKER
+  event(nullptr,"DETECTION_GRAPH","mux->nvinfer(YOLO26m,FP16,batch=6,interval=0)->DeepStream-NMS(iou=0.45,conf=0.25)->nvtracker(NvDCF_perf,960x544)->tiler->NVMM/RGBA->nvdsosd(GPU,person+ID)->NVENC inference=1 tracker=1");
+#else
   event(nullptr,"DETECTION_GRAPH","mux->nvinfer(YOLO26m,FP16,batch=6,interval=0)->DeepStream-NMS(iou=0.45,conf=0.25)->tiler->NVMM/RGBA->nvdsosd(GPU,person+confidence)->NVENC inference=1");
+#endif
 }
